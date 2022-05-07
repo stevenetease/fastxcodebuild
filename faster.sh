0e22cb69b5e53c7928465084f8114ceea635e554
许扬 <38337822+stevenetease@users.noreply.github.com>
2022-05-07 16:16:59 +0800

    Create faster.sh
    xcode setting

---
+++ faster.sh
@@ -0,0 +1,4 @@
+defaults write com.apple.dt.xcodebuild PBXNumberOfParallelBuildSubtasks 4
+defaults write com.apple.dt.xcodebuild IDEBuildOperationMaxNumberOfConcurrentCompileTasks 4
+defaults write com.apple.dt.Xcode PBXNumberOfParallelBuildSubtasks 4
+defaults write com.apple.dt.Xcode IDEBuildOperationMaxNumberOfConcurrentCompileTasks 4
